156f793f7aab8a0a1bd792c3c79e030649c5c5dc
Minhkun912 <minhnguyenduc506@gmail.com>
2023-03-15 16:33:03 +0700

    first commit

---
+++ src/bai2/Main.java
@@ -0,0 +1,11 @@
+package bai2;
+
+public class Main {
+    public static void main(String[] args) {
+        Point2D first2DPoint=new Point2D(3, 4);
+        Point3D first3DPoint=new Point3D(4, 5,7);
+
+        System.out.println(first2DPoint.toString());
+        System.out.println(first3DPoint.toString());
+    }
+}
